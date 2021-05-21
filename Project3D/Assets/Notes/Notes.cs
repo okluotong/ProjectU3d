b4ca1dbc04ffff8209baddacf246e9cb4ae8eb7f
lt <247376862@qq.com>
2021-05-21 10:13:27 +0800

    修正目录页

---
+++ Project3D/Assets/Notes/Notes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Notes
+{
+    class Notes
+    {
+        //UI框架介绍网站
+        //https://www.jianshu.com/p/666de6c7695a
+
+
+        //在上面的网址条件下，UI重构了下
+        //https://www.jianshu.com/p/b480a194f7d7
+
+
+
+        //Unity ToLua & LuaFramework_UGUI学习笔记
+        //https://www.jianshu.com/p/ef1e2641e0c6
+
+    }
+}
